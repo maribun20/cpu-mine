610d2203a120f266f759d2426627967fb5d59f88
maribun20 <68433096+maribun20@users.noreply.github.com>
2020-08-14 23:55:38 +0700

    Update conf-mining.sh

--- conf-mining.sh
+++ conf-mining.sh
@@ -1,2 +1,1 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u MAEMuVLuQabXnzZbcmsiY1UEdZ1WFVJLGZ -p c=LTC -t 8
-sleep 0
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz
