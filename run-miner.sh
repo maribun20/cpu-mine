89f2d246cdf4af61779054b182d1ebb8ca8adb5f
maribun20 <68433096+maribun20@users.noreply.github.com>
2020-08-18 00:10:30 +0700

    Update run-miner.sh

--- run-miner.sh
+++ run-miner.sh
@@ -1,1 +1,1 @@
-./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u MAEMuVLuQabXnzZbcmsiY1UEdZ1WFVJLGZ -p c=LTC -t 70
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u MAEMuVLuQabXnzZbcmsiY1UEdZ1WFVJLGZ -p c=LTC -t 20
