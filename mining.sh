720cf262353996add46d662db0711c4cb3e23287
maribun20 <68433096+maribun20@users.noreply.github.com>
2020-08-23 03:14:07 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,1 +1,1 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && chmod +x cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ls
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && chmod +x cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz
